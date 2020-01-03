6aa55a522574e1f72b58fe6ed140b8699e0ac962
Candice Chin <chincandice2007@gmail.com>
2020-01-03 13:55:00 -0800

    added starter sequelize code to setup schema and models

--- client/src/database/connection.js
+++ client/src/database/connection.js
@@ -22,3 +22,125 @@ sequelize.authenticate().complete(function (err) {
 
 module.exports = sequelize;
 global.sequelize = sequelize;
+
+//Starter code to create review model
+/*
+module.exports = sequelize.define("Review", {
+    id: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    content: Sequelize.STRING(300)
+  });
+*/
+
+//Starter code to create reviewer model
+/*
+module.exports = sequelize.define("Reviewer", {
+    id: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    username: {
+    type: Sequelize.STRING(35),
+    allowNull: false,
+    unique: true
+  },
+  passwd: {
+    type: Sequelize.STRING(20),
+    allowNull: false
+  }
+  });
+*/
+
+//Example actions for Sequelize using Node.js
+
+//Create Item Table Structure
+var Item = sequelize.define('Item', {
+    id: Sequelize.STRING,
+    name:Sequelize.STRING,
+    description: Sequelize.STRING,
+    qty: Sequelize.INTEGER
+});
+ 
+//Applying Item Table to database
+sequelize.sync({force:true}).complete(function (err) {
+ if(err){
+    console.log('An error occur while creating table');
+ }else{
+    console.log('Item table created successfully');
+ }
+});
+
+//There is two way of inserting data into database
+//One way: Forming object from modal
+var item1 = Item.build({
+    id: 1,
+    name:'Laptop',
+    description: 'Acer 2340TL',
+    qty: 23
+});
+//Inserting Data into database
+item1.save().complete(function (err) {
+ if (err) {
+    console.log('Error in Inserting Record');
+ } else {
+    console.log('Data successfully inserted');
+ }
+});
+ 
+//Other way: Immediate insertion of data into database
+sequelize.sync().success(function () {
+  Item.create({
+     id: 2,
+     name:'Cell Phone',
+     description: 'Sony',
+     qty: 20
+  }).success(function (data) {
+  console.log(data.values)
+ })
+});
+
+//Reading All Data
+Item.find({}).complete(function (err,data) {
+    console.log(data);
+});
+//With where condition
+    Item.find({where:{name:'Laptop'}}).complete(function (err, data) {
+   console.log(data);
+});
+
+//Updating Laptop to Computer
+Item.find({where:{name:'Laptop'}}).complete(function (err, data) {
+    if(err){
+      console.log(err);
+    }
+    if(data){
+      data.updateAttributes({
+      name:'Computer'
+    }).success(function (data1) {
+      console.log(data1);
+    })
+   }
+  });
+
+  //Delete All Computer Records
+Item.find({where: {name: 'Computer'}}).complete(function (err, data) {
+    if (err) {
+       console.log(err);
+    } else {
+       data.destroy({}).success(function (err, data) {
+    if(err){
+       console.log(err);
+    }else{
+       console.log(data);
+    }
+   })
+  }
+  console.log(data);
+ });
+
